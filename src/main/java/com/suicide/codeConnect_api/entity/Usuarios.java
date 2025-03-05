965344d27f0062fa7d5d9139dc924bda159ff73c
Lorenzo <lorenzokaio12@gmail.com>
2025-03-05 20:46:02 -0300

    Primeiro Commit, feito a configuração do ambiente de desenvolvimento e a criação da entidade Usuario

---
+++ src/main/java/com/suicide/codeConnect_api/entity/Usuarios.java
@@ -0,0 +1,53 @@
+package com.suicide.codeConnect_api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuarios implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuarios usuarios = (Usuarios) o;
+        return Objects.equals(id, usuarios.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" +
+                "id=" + id +
+                '}';
+    }
+}
