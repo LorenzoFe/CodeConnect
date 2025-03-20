8698903720bbff3e124e120fab2150bdcf37b25e
LorenzoFer <rafael.silva@ibrowse-sds.com.br>
2025-03-20 11:56:05 -0300

    Feito os recursos do usuario referente a buscar id e alterar senha.

--- src/main/java/com/suicide/codeConnect_api/service/UsuarioService.java
+++ src/main/java/com/suicide/codeConnect_api/service/UsuarioService.java
@@ -3,6 +3,9 @@ package com.suicide.codeConnect_api.service;
 import com.suicide.codeConnect_api.entity.Usuario;
 import com.suicide.codeConnect_api.repository.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,19 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
-
+    @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    @Transactional
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuario id=%s não encontrado", id))
+        );
+    }
+
+    @Transactional
     public Usuario autenticar(String email, String password) {
         Usuario usuario = usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
@@ -24,4 +35,18 @@ public class UsuarioService {
         }
         return usuario;
     }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
+            if (!novaSenha.equals(confirmaSenha)){
+                throw new RuntimeException("Nova senha está diferente da Confirma senha");
+            }
+            Usuario user = buscarPorId(id);
+            if (!user.getPassword().equals(senhaAtual)){
+                throw new RuntimeException("Senha atual errada");
+            }
+            user.setPassword(novaSenha);
+            return user;
+    }
+
 }
--- src/main/java/com/suicide/codeConnect_api/web/controller/UsuarioController.java
+++ src/main/java/com/suicide/codeConnect_api/web/controller/UsuarioController.java
@@ -7,15 +7,13 @@ import com.suicide.codeConnect_api.service.UsuarioService;
 import com.suicide.codeConnect_api.web.dto.LoginDto;
 import com.suicide.codeConnect_api.web.dto.UsuarioCreateDto;
 import com.suicide.codeConnect_api.web.dto.UsuarioResponseDto;
+import com.suicide.codeConnect_api.web.dto.UsuarioSenhaDto;
 import com.suicide.codeConnect_api.web.dto.mapper.UsuarioMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,10 +29,22 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
+        Usuario user =usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
+    }
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         Usuario usuario = usuarioService.autenticar(loginDto.getEmail(), loginDto.getPassword());
         return ResponseEntity.ok("Login realizado com sucesso");
     }
-    //proximo passo é criar o end point de login e tratar as exeções
+
+    @PatchMapping("/{id}")
+        public ResponseEntity<UsuarioResponseDto> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto){
+           Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+            return ResponseEntity.ok(UsuarioMapper.toDto(user));
+    }
+    //proximos passos tratar excções e documentar no swagger
 }
