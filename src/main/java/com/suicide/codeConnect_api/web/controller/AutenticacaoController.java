daba5fdfc2acfd6474941959d8138e163b24a60d
LorenzoFer <rafael.silva@ibrowse-sds.com.br>
2025-05-26 14:08:29 -0300

    Configurações JWT( incompleto ainda)

---
+++ src/main/java/com/suicide/codeConnect_api/web/controller/AutenticacaoController.java
@@ -0,0 +1,46 @@
+package com.suicide.codeConnect_api.web.controller;
+
+import com.suicide.codeConnect_api.jwt.JwtToken;
+import com.suicide.codeConnect_api.jwt.JwtUserDetailsService;
+import com.suicide.codeConnect_api.web.dto.LoginDto;
+import com.suicide.codeConnect_api.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class AutenticacaoController {
+
+    private final JwtUserDetailsService detailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginDto dto, HttpServletRequest request){
+        log.info("Processo de autenticação pelo login {}", dto.getEmail());
+        try{
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getEmail());
+            return ResponseEntity.ok(token);
+        }catch (AuthenticationException ex){
+            log.warn("Bad credentials from email '{}'",dto.getEmail());
+        }
+        return ResponseEntity
+                .badRequest().body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais invalidas!"));
+
+    }
+}
