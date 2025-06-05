134f4d400c18311f3d429a82ae93fbe3b07a95e2
Lorenzo <lorenzokaio12@gmail.com>
2025-06-05 17:02:08 -0300

    Fix(Entity Usuário)

--- src/main/java/com/suicide/codeConnect_api/entity/Usuario.java
+++ src/main/java/com/suicide/codeConnect_api/entity/Usuario.java
@@ -35,7 +35,7 @@ public class Usuario implements Serializable {
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao = LocalDateTime.now();;
     @Lob
-    @Column(name = "descricao", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "descricao", nullable = true, columnDefinition = "TEXT")
     private String descricao;
 
     @Column(name = "foto_url")
