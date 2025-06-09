28f61df99a418c2230c2b72ef49d4c5906f1be4c
Lorenzo <lorenzokaio12@gmail.com>
2025-06-09 17:55:07 -0300

    Configurando Deploy

--- src/main/java/com/suicide/codeConnect_api/config/SpringSecurityConfig.java
+++ src/main/java/com/suicide/codeConnect_api/config/SpringSecurityConfig.java
@@ -65,7 +65,8 @@ public class SpringSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5501"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5501",
+                "https://code-connect-two-gilt.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
